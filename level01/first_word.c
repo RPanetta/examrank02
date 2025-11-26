a57e30b12afa22b2f2a9bf566c8ffa1e64015840
Rocio Panetta <rpanetta@f0r1s10.codam.nl>
2025-11-26 12:38:30 +0100

    firstword

--- level01/first_word.c
+++ level01/first_word.c
@@ -6,24 +6,23 @@
 /*   By: rpanetta <rpanetta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/25 14:10:57 by rpanetta          #+#    #+#             */
-/*   Updated: 2025/11/26 12:19:45 by rpanetta         ###   ########.fr       */
+/*   Updated: 2025/11/26 12:36:38 by rpanetta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
 
-int	first_word(int argc, char **argv)
+void	first_word(int argc, char **argv)
 {
 	int i;
 
+	i = 0;
 	if (argc != 2)
 	{
 		write(1, "\n", 1);
-		return(0);
+		return;
 	}
-	
-	i = 0;
 	while (argv[1][i] == ' ' || argv[1][i] == '\t')
 		i++;
 	while (argv[1][i] != '\0' && argv[1][i] != ' ' && argv[1][i] != '\t')
@@ -32,13 +31,9 @@ int	first_word(int argc, char **argv)
 		i++;
 	}
 	write (1, "\n", 1);
-	return(0);
 }
-int main(void)
+int main(int argc, char **argv)
 {
-	char test1[] = "hello";
-	char test2[] = "FOR PONY";
-	first_word(1, test1);
-	first_word(2, test2);
+	first_word(argc, argv);
 	return(0);
 }
