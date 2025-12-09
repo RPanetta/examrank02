1df6d9042818b625ff2b83e049fbc3db4e23504b
Rocio Panetta <rpanetta@f0r1s10.codam.nl>
2025-12-09 15:48:36 +0100

    level02

--- level02/inter.c
+++ level02/inter.c
@@ -6,33 +6,63 @@
 /*   By: rpanetta <rpanetta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 16:56:54 by rpanetta          #+#    #+#             */
-/*   Updated: 2025/12/08 17:12:33 by rpanetta         ###   ########.fr       */
+/*   Updated: 2025/12/09 15:47:51 by rpanetta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_inter(int argc, char **argv)
+int	ft_char_in_str(char c, char *str)
 {
 	int	i;
 
 	i = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+		{
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	ft_already_printed(char c, char *str, int position)
+{
+	int	i;
+
+	i = 0;
+	while (i < position)
+	{
+		if (str[i] == c)
+		{
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+void	ft_inter(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 0;
 	if (argc == 3)
 	{
-		while(argv[1][i] != '\0' && argv[2][i] != '\0')
+		while(argv[1][i] != '\0')
 		{
-			if (argv[2][i] == argv[1][i])
+			if (ft_char_in_str(argv[1][i], argv[2]) &&
+			!ft_already_printed(argv[1][i], argv[1], i))
 			{
 				write(1, &argv[1][i], 1);
-				i++;
 			}
 			i++;
 		}
 	}
-	else
-	{
-		write(1, "\n", 1);
-	}
+	write(1, "\n", 1);
 }
 
 int	main(int argc, char **argv)
