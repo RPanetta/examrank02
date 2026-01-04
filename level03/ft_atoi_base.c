35f586d5cb2c6e01bca2add97bf8dd3e0d7f2143
RPanetta <panetta.rocio@gmail.com>
2026-01-04 15:34:01 +0100

    level03

--- level03/ft_atoi_base.c
+++ level03/ft_atoi_base.c
@@ -56,12 +56,72 @@ int	ft_atoi_base(const char *str, int str_base)
 	return (result * sign);
 }
 
-int	main(void)
-{
-	char	*str = "  -1234abcd";
-	int		result;
+// int	main(void)
+// {
+// 	char	*str = "  -1234abcd";
+// 	int		result;
 
-	result = ft_atoi_base(str, 16);
-	printf("%d\n", result);
-	return (0);
+// 	result = ft_atoi_base(str, 16);
+// 	printf("%d\n", result);
+// 	return (0);
+// }
+
+////////////////////////////////////////////////////////////////////////
+
+int	convert(char c)
+{
+	if (c - '0' <= 9 && c - '0'>= 0)
+	{
+		return (c - '0');
+	}
+	else if (c >= 'a' && c <= 'f')
+	{
+		return (c - 'a' + 10);
+	}
+	else if (c >= 'A' && c <= 'F')
+	{
+		return (c - 'A' + 10);
+	}
+	else
+		return (-1);
 }
+
+int	ft_atoi_base(const char *str, int str_base)
+{
+	int	i;
+	int	sign;
+	int	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
+		|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
+	{
+		i++;
+	}
+	if (str[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	while (str[i])
+	{
+		if (convert(str[i]) == -1)
+		{
+			break ;
+		}
+		result = result * str_base + convert(str[i]);
+		i++;
+	}
+	return (result * sign);
+}
+
+// #include <stdio.h>
+// int	main(void)
+// {
+// 	char 	*str = "-1A3  ";
+// 	printf("%d\n", ft_atoi_base(str, 15));
+// 	printf("%d\n", ft_atoi_base(str, 16));
+// 	return (0);
+// }
