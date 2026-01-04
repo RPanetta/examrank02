690dc4c5230fa7b289ad686cb3a9f1e317a87121
RPanetta <panetta.rocio@gmail.com>
2026-01-04 20:00:17 +0100

    new code

--- level03/expand_str.c
+++ level03/expand_str.c
@@ -12,42 +12,42 @@
 
 #include <unistd.h>
 
-void	expand_str(int argc, char **argv)
+void	expand_str(char *str)
 {
 	int	i;
 
 	i = 0;
-	if (argc == 2)
+	while (str[i] == '\t' || str[i] == ' ')
 	{
-		while (argv[1][i] == '\t' || argv[1][i] == ' ')
+		i++;
+	}
+	while (str[i] != '\0')
+	{
+		if (str[i] == '\t' || str[i] == ' ')
 		{
-			i++;
-		}
-		while (argv[1][i] != '\0')
-		{
-			if (argv[1][i] == '\t' || argv[1][i] == ' ')
+			while (str[i] == '\t' || str[i] == ' ')
 			{
-				while (argv[1][i] == '\t' || argv[1][i] == ' ')
-				{
-					i++;
-				}
-				if (argv[1][i] != '\0')
-				{
-					write(1, "   ", 3);
-				}
-			}
-			else
-			{
-				write(1, &argv[1][i], 1);
 				i++;
 			}
+			if (str[i] != '\0')
+			{
+				write(1, "   ", 3);
+			}
+		}
+		else
+		{
+			write(1, &str[i], 1);
+			i++;
 		}
 	}
-	write (1, "\n", 1);
 }
 
 int	main(int argc, char **argv)
 {
-	expand_str(argc, argv);
+	if (argc == 2)
+	{
+		expand_str(argv[1]);
+	}
+	write(1, "\n", 1);
 	return (0);
 }
