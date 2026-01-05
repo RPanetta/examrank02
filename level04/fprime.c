9a67696a0bda41a7fa308b1ab629d129d186a284
RPanetta <panetta.rocio@gmail.com>
2026-01-05 15:02:23 +0100

    fprime

--- level04/fprime.c
+++ level04/fprime.c
@@ -6,44 +6,45 @@
 /*   By: rpanetta <rpanetta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/22 14:16:41 by rpanetta          #+#    #+#             */
-/*   Updated: 2025/12/30 13:35:08 by rpanetta         ###   ########.fr       */
+/*   Updated: 2026/01/05 15:00:47 by rpanetta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-void	fprime(int argc, char **argv)
+void	fprime(int n)
 {
-	int	n;
 	int	i;
 
 	i = 2;
-	if (argc == 2)
+	if (n == 1)
+		printf("1");
+	while (i <= n)
 	{
-		n = atoi(argv[1]);
-		if (n == 1)
-			printf("1");
-		while (i <= n)
+		if (n % i == 0)
 		{
-			if (n % i == 0)
-			{
-				printf("%d", i);
-				if (n != i)
-					printf("*");
-				n = n / i;
-			}
-			else
-			{
-				i++;
-			}
+			printf("%d", i);
+			if (n != i)
+				printf("*");
+			n = n / i;
+		}
+		else
+		{
+			i++;
 		}
 	}
-	printf("\n");
 }
 
 int	main(int argc, char **argv)
 {
-	fprime(argc, argv);
+	int	nbr;
+
+	if (argc == 2)
+	{
+		nbr = atoi(argv[1]);
+		fprime(nbr);
+	}
+	printf("\n");
 	return (0);
 }
