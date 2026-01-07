2bdd92404ba73a4161ac10a5d3997f393b58e79f
Rocio Panetta <rpanetta@f0r1s14.codam.nl>
2026-01-07 17:27:53 +0100

    level02

--- level02/is_power_of_2.c
+++ level02/is_power_of_2.c
@@ -6,24 +6,30 @@
 /*   By: rpanetta <rpanetta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/10 15:41:01 by rpanetta          #+#    #+#             */
-/*   Updated: 2025/12/12 13:18:25 by rpanetta         ###   ########.fr       */
+/*   Updated: 2026/01/07 17:27:23 by rpanetta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
+// int	is_power_of_2(unsigned int n)
+// {
+// 	if (n == 0)
+// 		return (0);
+// 	return ((n & (n - 1)) == 0);
+// }
+
 int	is_power_of_2(unsigned int n)
 {
-	int	test;
-
-	test = 1;
-	while (test <= n)
+	if (n == 0)
+		return (0);
+	while (n > 1)
 	{
-		if (test == n)
-			return (1);
-		test = test * 2;
+		if (n % 2 != 0)
+			return (0);
+		n = n / 2;
 	}
-	return (0);
+	return (1);
 }
 
 // int	main(void)
